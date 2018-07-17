5e20bc15351285b3de6b85875a9f92e076154432
danding_ge <danding_ge@126.com>
2018-07-17 23:46:02 +0800

    整理资源包

---
+++ src/App.js
@@ -0,0 +1,113 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+import * as WeChat from './wechat';
+let resolveAssetSource = require('resolveAssetSource');
+
+export default class App extends Component {
+  componentDidMount() {
+    // WeChat.registerApp('appid');
+  };
+
+  _sendAuthRequest = () => {
+    let scope = 'snsapi_userinfo';
+    let state = 'wechat_sdk_demo';
+    //判断微信是否安装
+    WeChat.isWXAppInstalled()
+      .then((isInstalled) => {
+        if (isInstalled) {
+          //发送授权请求
+          WeChat.sendAuthRequest(scope, state)
+            .then(responseCode => {
+              //todo: 返回code码，通过code获取access_token
+              // this.getAccessToken(responseCode.code);
+            })
+            .catch(err => {
+              console.log('登录授权发生错误：', err.message);
+            })
+        } else {
+          console.log('没有安装微信，请先安装微信客户端在进行登录');
+        }
+      })
+  };
+
+  _shareToSession = () => {
+    //判断微信是否安装
+    WeChat.isWXAppInstalled()
+      .then((isInstalled) => {
+        if (isInstalled) {
+          //发送授权请求
+          let imageResource = require('./icon64_wx_logo.png');
+          WeChat.shareToSession({
+            type: 'miniProgram',
+            title: '微信小程序消息',
+            description: '发送微信小程序消息给好友',
+            mediaTagName: '小程序消息',
+            messageAction: undefined,
+            messageExt: undefined,
+            imageUrl: resolveAssetSource(imageResource).uri
+          })
+          .then(result => {
+            console.log('成功发送微信小程序消息给好友', result);
+          })
+          .catch(err => {
+            console.log('登录授权发生错误：', err.message);
+          })
+        } else {
+          console.log('没有安装微信，请先安装微信客户端在进行登录');
+        }
+      })
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to WeChat for Android!
+        </Text>
+        {/* 微信登录demo */}
+        <TouchableOpacity onPress={() => {this._sendAuthRequest()}}>
+          <Text style={styles.instructions}>
+            To Login
+          </Text>
+        </TouchableOpacity>
+        {/* 发送小程序消息到好友 */}
+        <TouchableOpacity onPress={() => {this._shareToSession()}}>
+          <Text style={styles.instructions}>
+            To Share Mini
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
